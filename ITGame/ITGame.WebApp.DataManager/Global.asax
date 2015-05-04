172b39aa1be773d839d8c75aa85c3c6d7c805cdc
Aliaksei Piarouski <perovskijab@gmail.com>
2015-05-04 22:47:05 +0300

    default MVC web application with custom connection strings in the file web.config

---
+++ ITGame/ITGame.WebApp.DataManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITGame.WebApp.DataManager.MvcApplication" Language="C#" %>
---
+++ ITGame/ITGame.WebApp.DataManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITGame.WebApp.DataManager.Startup))]
+namespace ITGame.WebApp.DataManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
